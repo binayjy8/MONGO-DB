af9822fa5e830e7bdc4940c6ce9b58b411bf344d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-01 07:16:41 +0530

    backend database

--- books.js
+++ books.js
@@ -9,3 +9,15 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
+
+const bookSchema = new mongoose.Schema ({
+    title : {
+        type : String,
+    },
+    author : {
+        type : String,
+    },
+    price : {
+        type : Number,
+    },
+});
