28cc2475c80fde071e26d0c9a64022bbc712a8a6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-02 09:19:17 +0530

    backend code

--- books.js
+++ books.js
@@ -39,7 +39,7 @@ Book.findByIdAndUpdate("665b2e9718badc0866d0a2fe",
 {price : -200}, {runValidators: true}).then((res)=>{
     console.log(res);
 }).catch((err)=>{
-    console.log(err);
+    console.log(err.errors.price.properties.message);
 });
 
 // let book1 = new Book({
